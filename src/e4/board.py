1a55ff3491c6292c63718f279f4202980848ad7b
Trevor Bayless <trevorbayless1@gmail.com>
2021-02-25 11:25:35 -0600

    Break out into base class

--- src/e4/board.py
+++ src/e4/board.py
@@ -1,76 +1,82 @@
 from .config import config
 import chess
 
-class BoardDisplay:
-    '''Class to manage a chess boards display'''
-    def __init__(self, fen, perspective):
-        '''Initialize the class'''
+def get_square_numbers(orientation):
+    '''Returns the square numbers as a list based on board orientation'''
+    square_numbers = []
+    square_names = []
+
+    for rank in range(len(chess.RANK_NAMES)-1, -1, -1):
+        for file in range(len(chess.FILE_NAMES)):
+            square_index = chess.square(file, rank)
+            square_names.append(chess.square_name(square_index))
+            square_numbers.append(chess.square(file, rank))
+
+    if orientation == "black":
+        return square_numbers[::-1]
+    else:
+        return square_numbers
+
+
+def get_file_names(orientation):
+    '''Return the file names as a list based on board orientation'''
+    if orientation == "white":
+        return chess.FILE_NAMES
+    else:
+        return chess.FILE_NAMES[::-1]
+
+
+def get_rank_names(orientation):
+    '''Return the rank names as a list based on board orientation'''
+    if orientation == "white":
+        return chess.RANK_NAMES[::-1]
+    else:
+        return chess.RANK_NAMES
+
+
+class BoardBase:
+    '''Base board class'''
+    def __init__(self, orientation, fen=chess.STARTING_FEN):
         self.board = chess.Board(fen)
-        self.perspective = perspective
-        self.square_numbers = self.get_square_numbers()
+        self.orientation = orientation
+        self.square_numbers = get_square_numbers(self.orientation)
+        self.file_names = get_file_names(self.orientation)
+        self.rank_names = get_rank_names(self.orientation)
 
 
-    def set_board_perspective(self):
-        '''Returns the perspective of the board to display'''
-        return "white"
+    def set_orientation(self, orientation):
+        '''Sets the board orientation'''
+        self.orientation = orientation
 
 
-    def perspective_is_white(self):
-        '''Returns True is the board perspective is set as "white"'''
-        if self.perspective == "white":
+    def is_white_orientation(self):
+        '''Returns true if the board orientation is set as white'''
+        if self.orientation == "white":
             return True
         else:
             return False
 
 
-    def get_square_numbers(self):
-        '''Returns the square numbers in a list based on board orientation'''
-        square_numbers = []
-        square_names = []
-
-        for rank in range(len(chess.RANK_NAMES)-1, -1, -1):
-            for file in range(len(chess.FILE_NAMES)):
-                square_index = chess.square(file, rank)
-                square_names.append(chess.square_name(square_index))
-                square_numbers.append(chess.square(file, rank))
-
-        if self.perspective_is_white():
-            return square_numbers
-        else:
-            return square_numbers[::-1]
-
-
-    def get_rank_names(self):
-        '''Return the rank name list based on board perspective'''
-        if self.perspective_is_white():
-            return chess.RANK_NAMES[::-1]
-        else:
-            return chess.RANK_NAMES
-
-
-    def get_file_names(self):
-        '''Return the file name list based on board perspective'''
-        if self.perspective_is_white():
-            return chess.FILE_NAMES
-        else:
-            return chess.FILE_NAMES[::-1]
+class Board(BoardBase):
+    '''Class to manage a chess board display'''
+    def __init__(self, orientation, fen=chess.STARTING_FEN):
+        '''Initialize the class'''
+        super().__init__(orientation, fen)
 
 
     def draw_board(self):
-        '''Draws the chess board'''
+        '''Draws the board based on stored positions'''
         rank_count = 0
         count = 0
         new_line = True
         board_output = ""
-        rank_names = self.get_rank_names()
-        file_names = self.get_file_names()
         show_board_coordinates = config.get_board_boolean(config.BoardKeys.SHOW_BOARD_COORDINATES)
         blindfold_mode = config.get_board_boolean(config.BoardKeys.BLINDFOLD_CHESS)
         use_unicode_pieces = config.get_board_boolean(config.BoardKeys.USE_UNICODE_PIECES)
 
         for square in self.square_numbers:
             if show_board_coordinates and new_line:
-                board_output += rank_names[rank_count] + " "
+                board_output += self.rank_names[rank_count] + " "
 
             piece = self.board.piece_at(square)
             if piece:
@@ -94,7 +100,7 @@ class BoardDisplay:
 
         if show_board_coordinates:
             board_output += "  "
-            for file_name in file_names:
+            for file_name in self.file_names:
                 board_output += file_name + " "
 
         print(board_output + "\n")
