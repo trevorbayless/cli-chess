d218f7b8f86762270dc4971cb9804314a93d0534
Trevor Bayless <trevorbayless1@gmail.com>
2021-03-01 13:13:05 -0600

    Allow functions to be self-sustaining for easier testing

--- src/e4/board.py
+++ src/e4/board.py
@@ -11,8 +11,8 @@ UNICODE_PIECE_SYMBOLS = {
     "p": "♟",
 }
 
-def get_square_numbers(orientation):
-    '''Returns the square numbers as a list based on board orientation'''
+def get_board_squares(orientation):
+    '''Returns the boards square numbers as a list based on board orientation'''
     square_numbers = []
     square_names = []
 
@@ -33,38 +33,78 @@ def get_piece_unicode_symbol(symbol):
     return UNICODE_PIECE_SYMBOLS[symbol.lower()]
 
 
-class BoardBase:
-    '''Base board class'''
+class Board:
+    '''Class to manage a chess board display'''
     def __init__(self, orientation, fen=chess.STARTING_FEN):
+        '''Initialize the class'''
         self.board = chess.Board(fen)
-        self.orientation = orientation
-        self.square_numbers = get_square_numbers(self.orientation)
+        self.board_orientation = orientation
+        self.board_squares = get_board_squares(self.board_orientation)
+        self.board_display = self.generate_board()
 
 
-    def set_orientation(self, orientation):
-        '''Sets the board orientation'''
-        self.orientation = orientation
+    def generate_board(self):
+        '''Generates and returns the board as a HTML string (top left to bottom right)
+           based on the board orientation, and stored piece positions
+        '''
+        board_output = ""
+        for square in self.board_squares:
+            board_output += self.get_rank_label(square)
+            board_output += self.get_squares_final_display(square)
+            board_output += self.start_new_line(square)
+
+        board_output += self.get_file_labels() + "\n"
+
+        return board_output
 
 
-    def is_white_orientation(self):
-        '''Returns true if the board orientation is set as white'''
-        if self.orientation == "white":
-            return True
+    def print_board(self):
+        '''Prints the board'''
+        print(HTML(self.board_display))
+
+
+    def get_squares_final_display(self, square):
+        '''Returns as a HTML string containing the final display for the passed in square.
+           This includes the square color, and piece within the square.
+        '''
+        piece = self.board.piece_at(square)
+        square_color = self.get_square_color(square)
+        square_output = ""
+
+        blindfold_chess = config.get_board_boolean(config.BoardKeys.BLINDFOLD_CHESS)
+        use_unicode_pieces = config.get_board_boolean(config.BoardKeys.USE_UNICODE_PIECES)
+
+        if piece and not blindfold_chess:
+            piece_color = self.get_piece_color(piece)
+            piece_character = get_piece_unicode_symbol(piece.symbol()) if use_unicode_pieces else piece.symbol()
+            square_output = f"<style fg='{piece_color}' bg='{square_color}'>{piece_character} </style>"
         else:
-            return False
+            square_output = f"<style bg='{square_color}'>  </style>"
+
+        return square_output
 
 
-    def get_piece_display_color(self, piece):
-        '''Returns the color to display the piece as a string'''
+    def get_piece_color(self, piece):
+        '''Returns a string with the color to display the
+           piece as based on configuration settings
+        '''
+        piece_color = ""
         base_is_white = piece.color == True
         if base_is_white:
-            return config.get_board_value(config.BoardKeys.LIGHT_PIECE_COLOR)
+            piece_color = config.get_board_value(config.BoardKeys.LIGHT_PIECE_COLOR)
         else:
-            return config.get_board_value(config.BoardKeys.DARK_PIECE_COLOR)
+            piece_color = config.get_board_value(config.BoardKeys.DARK_PIECE_COLOR)
+
+        return piece_color
 
 
-    def get_square_display_color(self, file_index, rank_index):
-        '''Returns the color to display the square as a string'''
+    def get_square_color(self, square):
+        '''Returns a string with the color to display the
+           square based on configuration settings
+        '''
+        file_index = chess.square_file(square)
+        rank_index = chess.square_rank(square)
+
         is_light_square = (file_index % 2) != (rank_index % 2)
         if is_light_square:
             return config.get_board_value(config.BoardKeys.LIGHT_SQUARE_COLOR)
@@ -72,47 +112,34 @@ class BoardBase:
             return config.get_board_value(config.BoardKeys.DARK_SQUARE_COLOR)
 
 
-class Board(BoardBase):
-    '''Class to manage a chess board display'''
-    def __init__(self, orientation, fen=chess.STARTING_FEN):
-        '''Initialize the class'''
-        super().__init__(orientation, fen)
+    def get_file_labels(self):
+        '''Returns a string containing the file labels depending
+           on the rank index and configuration settings
+        '''
+        file_labels = ""
+        show_board_coordinates = config.get_board_boolean(config.BoardKeys.SHOW_BOARD_COORDINATES)
 
-
-    def draw_board(self):
-        '''Draws the board (top left to bottom right) based on stored positions and orientation'''
-        blindfold_chess = config.get_board_boolean(config.BoardKeys.BLINDFOLD_CHESS)
-        use_unicode_pieces = config.get_board_boolean(config.BoardKeys.USE_UNICODE_PIECES)
-        board_output = ""
-
-        for square in self.square_numbers:
-            file_index = chess.square_file(square)
-            rank_index = chess.square_rank(square)
-            square_color = self.get_square_display_color(file_index, rank_index)
-            piece = self.board.piece_at(square)
-            square_output = ""
-
-            if piece and not blindfold_chess:
-                piece_color = self.get_piece_display_color(piece)
-                piece_character = get_piece_unicode_symbol(piece.symbol()) if use_unicode_pieces else piece.symbol()
-                square_output = f"<style fg='{piece_color}' bg='{square_color}'>{piece_character} </style>"
+        if show_board_coordinates:
+            file_labels = "  "
+            if self.board_orientation == "black":
+                for name in chess.FILE_NAMES[::-1]:
+                    file_labels += name + " "
             else:
-                square_output = f"<style bg='{square_color}'>  </style>"
+                for name in chess.FILE_NAMES:
+                    file_labels += name + " "
 
-            board_output += self.get_rank_label(file_index, rank_index)
-            board_output += square_output
-            board_output += self.start_new_line(file_index)
-
-        board_output += self.get_file_labels() + "\n"
-
-        print(HTML(board_output))
+        return file_labels
 
 
-    def get_rank_label(self, file_index, rank_index):
-        '''Add a rank label if needed'''
+    def get_rank_label(self, square):
+        '''Returns a string with the rank label at the
+           square passed in. Return is based on '''
         rank_label = ""
         proper_file_index = False
         show_board_coordinates = config.get_board_boolean(config.BoardKeys.SHOW_BOARD_COORDINATES)
+
+        file_index = chess.square_file(square)
+        rank_index = chess.square_rank(square)
 
         if self.is_white_orientation() and file_index == 0:
             proper_file_index = True
@@ -125,33 +152,24 @@ class Board(BoardBase):
         return rank_label
 
 
-    def get_file_labels(self):
-        '''Returns a string containing the file labels depending
-           on the rank index and configuration settings
-        '''
-        file_labels = ""
-        show_board_coordinates = config.get_board_boolean(config.BoardKeys.SHOW_BOARD_COORDINATES)
-
-        if show_board_coordinates:
-            file_labels = "  "
-            if self.orientation == "black":
-                for name in chess.FILE_NAMES[::-1]:
-                    file_labels += name + " "
-            else:
-                for name in chess.FILE_NAMES:
-                    file_labels += name + " "
-
-        return file_labels
-
-
-    def start_new_line(self, file_index):
+    def start_new_line(self, square):
         '''Returns a new line if the board output needs to start on a new
            line based on the board orientation and file index
         '''
         output = ""
+        file_index = chess.square_file(square)
+
         if self.is_white_orientation() and file_index == 7:
             output = "\n"
         elif not self.is_white_orientation() and file_index == 0:
             output = "\n"
 
         return output
+
+
+    def is_white_orientation(self):
+        '''Returns True if the board orientation is set as white'''
+        if self.board_orientation == "white":
+            return True
+        else:
+            return False
