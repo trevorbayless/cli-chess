376c62124a8c794b1fbad7700bc5dcc6e8e7de4b
Trevor Bayless <trevorbayless1@gmail.com>
2021-03-05 17:08:13 -0600

    Add function return value annotations

--- src/e4/config.py
+++ src/e4/config.py
@@ -5,48 +5,48 @@ import configparser
 #TODO: Handle new config options on updates (do not overwrite full config)
 
 class BaseConfig:
-    def __init__(self, full_filename):
+    def __init__(self, full_filename) -> None:
         """Default base class constructor"""
         self.filename = full_filename
         self.parser = configparser.ConfigParser()
 
 
-    def read_config(self):
+    def read_config(self) -> bool:
         """Attempts to read the configuration file (True if file exists)"""
         self.parser.read(self.filename)
         return self.config_exists()
 
 
-    def write_config(self):
+    def write_config(self) -> None:
         """Writes to the configuration file"""
         with open(self.filename, 'w') as config_file:
             self.parser.write(config_file)
 
 
-    def config_exists(self):
+    def config_exists(self) -> None:
         """Returns True if the configuration file exists"""
         return os.path.isfile(self.filename)
 
 
-    def add_section(self, section):
+    def add_section(self, section) -> None:
         """Add a section to the configuration file"""
         self.parser[section] = {}
         self.write_config()
 
 
-    def set_key_value(self, section, key, value):
+    def set_key_value(self, section, key, value) -> None:
         """Set (or add) a key/value to a section in the configuration file"""
         #TODO: Raise error if section does not exist
         self.parser[section][key] = value
         self.write_config()
 
 
-    def get_config_filename(self):
+    def get_config_filename(self) -> str:
         """Returns the configuration filename"""
         return self.filename
 
 
-    def get_key_value(self, section, key, lowercase=True):
+    def get_key_value(self, section, key, lowercase=True) -> str:
         """Retrieve the value at the passed in section/key pair"""
         try:
             key_value = self.parser.get(section, key).strip()
@@ -57,7 +57,7 @@ class BaseConfig:
             self.handle_exception(e)
 
 
-    def get_key_boolean_value(self, section, key):
+    def get_key_boolean_value(self, section, key) -> bool:
         """Retrieve the boolean value at the passed in section/key pair"""
         try:
             return self.parser.getboolean(section, key)
@@ -65,7 +65,7 @@ class BaseConfig:
             self.handle_exception(e)
 
 
-    def handle_exception(self, e):
+    def handle_exception(self, e) -> None:
         """Handles exceptions that occur while parsing the configuration file"""
         #TODO: Handle base class exceptions
         print("Exception caught while handling the configuration file. Please recreate the configuration file.")
@@ -73,7 +73,7 @@ class BaseConfig:
 
 class Config(BaseConfig):
     """Class to create, update, and read from the configuration file"""
-    def __init__(self, full_filename):
+    def __init__(self, full_filename) -> None:
         """Default constructor. Calls the base class to read the configuration
            file. If the file does not exist, the default configuration is created.
         """
@@ -82,7 +82,7 @@ class Config(BaseConfig):
             self.create_default_config()
 
 
-    def create_default_config(self, overwrite=False):
+    def create_default_config(self, overwrite=False) -> None:
         """Creates the default configuration file"""
         if not super().config_exists() or overwrite:
             self.create_board_section()
@@ -90,7 +90,7 @@ class Config(BaseConfig):
             self.create_lichess_section()
 
 
-    def create_board_section(self):
+    def create_board_section(self) -> None:
         """Creates the default 'BOARD' section in the config file"""
         is_unix = os.name == "posix"
         super().add_section(Config.Sections.BOARD)
@@ -108,69 +108,59 @@ class Config(BaseConfig):
         self.set_board_value(Config.BoardKeys.DARK_PIECE_COLOR, "black")
 
 
-    def set_board_value(self, key, value):
+    def set_board_value(self, key, value) -> None:
         """Modify (or add) a keys value at the 'BOARD' section"""
         super().set_key_value(Config.Sections.BOARD, key, value)
 
 
-    def get_board_value(self, key):
+    def get_board_value(self, key, lowercase=True) -> bool:
         """Returns a value from the 'BOARD' section at the passed in key"""
-        return super().get_key_value(Config.Sections.BOARD, key)
+        return super().get_key_value(Config.Sections.BOARD, key, lowercase)
 
 
-    def get_board_boolean(self, key):
+    def get_board_boolean(self, key) -> bool:
         """Returns a boolean value from the 'BOARD' section at the passed in key"""
         return super().get_key_boolean_value(Config.Sections.BOARD, key)
 
 
-    def create_ui_section(self):
+    def create_ui_section(self) -> None:
         """Creates the default 'UI' section in the config file"""
         super().add_section(Config.Sections.UI)
         self.set_ui_value(Config.UiKeys.ZEN_MODE, "no")
 
 
-    def set_ui_value(self, key, value):
+    def set_ui_value(self, key, value) -> None:
         """Modify (or add) a keys value at the 'UI' section"""
         super().set_key_value(Config.Sections.UI, key, value)
 
 
-    def get_ui_value(self, key):
+    def get_ui_value(self, key, lowercase=True) -> str:
         """Returns a value from the 'UI' section at the passed in key"""
-        return super().get_key_value(Config.Sections.UI, key)
+        return super().get_key_value(Config.Sections.UI, key, lowercase)
 
 
-    def get_ui_boolean(self, key):
+    def get_ui_boolean(self, key) -> bool:
         """Returns a boolean value from the 'UI' section at the passed in key"""
         return super().get_key_boolean_value(Config.Sections.UI, key)
 
 
-    def create_lichess_section(self):
+    def create_lichess_section(self) -> None:
         """Creates the default 'LICHESS' section in the config file"""
         super().add_section(Config.Sections.LICHESS)
         self.set_lichess_value(Config.LichessKeys.API_TOKEN, "")
 
 
-    def set_lichess_value(self, key, value):
+    def set_lichess_value(self, key, value) -> None:
         """Modify (or add) a keys value at the 'LICHESS' section at the passed in key"""
         super().set_key_value(Config.Sections.LICHESS, key, value)
 
 
-    def get_lichess_value(self, key):
+    def get_lichess_value(self, key, lowercase=True) -> str:
         """Returns a value from the 'LICHESS' section at the passed in key"""
-        return super().get_key_value(Config.Sections.LICHESS, key)
+        return super().get_key_value(Config.Sections.LICHESS, key, lowercase)
 
 
-    def get_lichess_api_token(self):
-        """Returns the stored lichess api token"""
-        api_token = super().get_key_value(Config.Sections.LICHESS, Config.LichessKeys.API_TOKEN, False)
-
-        if api_token != "":
-            return api_token
-        else:
-            return None
-
-
-    def get_lichess_boolean(self, key):
+    def get_lichess_boolean(self, key) -> bool:
         """Returns a boolean value from the UI section at the passed in key"""
         return super().get_key_boolean_value(Config.Sections.LICHESS, key)
 
