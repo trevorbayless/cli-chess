bbce93161b4b8c37b979f7e6cc5eae76b24aba01
Trevor Bayless <trevorbayless1@gmail.com>
2021-11-03 17:15:57 -0500

    Minor changes to presenter logic

--- src/cli_chess/game/game_presenter.py
+++ src/cli_chess/game/game_presenter.py
@@ -1,17 +1,21 @@
 from .game_model import GameModel
+from .game_view import *
 
 class GamePresenter:
     """ Mediator between the GameModel and the GameView.
         Takes events from GameModel and GameView and translates
         into actions each can understand.
     """
-    def __init__(self):
-        self.game_model = GameModel()
+    def __init__(self, model : GameModel):
+        self.game_model = model
+        self.game_view = GameView(self)
+        self.initialize_view()
 
 
-    def get_board_output(self) -> str:
-        return self.game_model.board.get_board_display()
+    def initialize_view(self):
+        self.game_view.update_board_output(self.game_model.board.get_board_display())
 
 
-    def receive_input(self, input):
+    def input_received(self, input):
         self.game_model.make_move(input)
+        self.game_view.update_board_output(self.game_model.board.get_board_display())
