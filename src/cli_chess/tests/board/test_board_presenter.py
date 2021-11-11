b57e5dd4c088857fd73bb4be517c3d0068875d0c
Trevor Bayless <trevorbayless1@gmail.com>
2021-11-10 21:10:35 -0600

    Setup initial unit test framework

---
+++ src/cli_chess/tests/board/test_board_presenter.py
@@ -0,0 +1,34 @@
+import unittest
+from cli_chess.board.board_model import BoardModel
+from cli_chess.board.board_presenter import BoardPresenter
+
+class BoardPresenterTestCase(unittest.TestCase):
+    def test_get_view(self):
+        pass
+
+    def test_make_move(self):
+        pass
+
+    def test_update_board(self):
+        pass
+
+    def test_get_file_labels(self):
+        pass
+
+    def test_apply_rank_label(self):
+        pass
+
+    def test_get_square_final_display(self):
+        pass
+
+    def test_start_new_line(self):
+        pass
+
+    def test_get_piece_color(self):
+        pass
+
+    def test_get_square_display_color(self):
+        pass
+
+    def test_game_result(self):
+        pass
