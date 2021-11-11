dd8b3b82b3f26976fcafa1048126111b8d52dcc5
Trevor Bayless <trevorbayless1@gmail.com>
2021-11-10 21:17:37 -0600

    Initial unit test stub for the board model class

---
+++ src/cli_chess/tests/board/test_board_model.py
@@ -0,0 +1,51 @@
+import unittest
+from cli_chess.board.board_model import BoardModel
+
+class BoardModelTestCase(unittest.TestCase):
+    def test_make_move(self):
+        pass
+
+    def test_set_board_orientation(self):
+        pass
+
+    def test_get_board_orientation(self):
+        pass
+
+    def test_get_board(self):
+        pass
+
+    def test_get_board_squares(self):
+        pass
+
+    def test_get_piece_at_square(self):
+        pass
+
+    def test_get_square_file(self):
+        pass
+
+    def test_get_square_rank(self):
+        pass
+
+    def test_get_file_labels(self):
+        pass
+
+    def test_get_rank_label(self):
+        pass
+
+    def test_get_previous_move(self):
+        pass
+
+    def test_is_square_in_check(self):
+        pass
+
+    def test_is_white_orientation(self):
+        pass
+
+    def test_get_game_over_result(self):
+        pass
+
+    def test_is_checkmate(self):
+        pass
+
+    def test_is_stalemate(self):
+        pass
