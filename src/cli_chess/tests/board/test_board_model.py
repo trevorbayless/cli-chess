29374269b19a5ac006f150a7f3e49fc2230dc846
Trevor Bayless <trevorbayless1@gmail.com>
2021-11-12 19:35:06 -0600

    Added some unit tests

--- src/cli_chess/tests/board/test_board_model.py
+++ src/cli_chess/tests/board/test_board_model.py
@@ -1,51 +1,71 @@
 import unittest
+
+from chess import Board
 from cli_chess.board.board_model import BoardModel
 
+
 class BoardModelTestCase(unittest.TestCase):
+
     def test_make_move(self):
-        pass
+        model = BoardModel()
+
+        # Valid standard move
+        move = model.make_move("Nf3")
+        self.assertEqual(model.board.peek(), move)
+
+        # Illegal standard move
+        with self.assertRaises(ValueError):
+            move = model.make_move("Qe6")
+
 
     def test_set_board_orientation(self):
-        pass
+        model = BoardModel()
+        self.assertEqual(model.get_board_orientation(), "white")
+
+        model.set_board_orientation("black")
+        self.assertEqual(model.get_board_orientation(), "black")
+
 
     def test_get_board_orientation(self):
-        pass
+        model = BoardModel("black")
+        self.assertEqual(model.get_board_orientation(), "black")
 
-    def test_get_board(self):
-        pass
+        model = BoardModel("white")
+        self.assertEqual(model.get_board_orientation(), "white")
+
+        model.set_board_orientation("black")
+        self.assertEqual(model.get_board_orientation(), "black")
+
 
     def test_get_board_squares(self):
         pass
 
-    def test_get_piece_at_square(self):
-        pass
 
     def test_get_square_file(self):
         pass
 
+
     def test_get_square_rank(self):
         pass
+
 
     def test_get_file_labels(self):
         pass
 
+
     def test_get_rank_label(self):
         pass
 
-    def test_get_previous_move(self):
-        pass
 
     def test_is_square_in_check(self):
-        pass
+        in_check_fen = "8/8/8/8/6K1/8/8/4Q1k1 b - - 21 61"
+        model = BoardModel(fen=in_check_fen)
+        self.assertTrue(model.is_square_in_check(model.board.king(model.board.turn)))
+
+        not_in_check_fen = "8/8/8/3Q4/8/6K1/8/6k1 w - - 21 61"
+        model.board.set_fen(not_in_check_fen)
+        self.assertFalse(model.is_square_in_check(model.board.king(model.board.turn)))
+
 
     def test_is_white_orientation(self):
-        pass
-
-    def test_get_game_over_result(self):
-        pass
-
-    def test_is_checkmate(self):
-        pass
-
-    def test_is_stalemate(self):
         pass
