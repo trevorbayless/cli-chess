21a4f8d1729e6f22b3e80d881bcedc124a941b74
Trevor Bayless <trevorbayless1@gmail.com>
2022-01-15 16:49:44 -0600

    Add notice

--- src/cli_chess/game/move_list/move_list_presenter.py
+++ src/cli_chess/game/move_list/move_list_presenter.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2021-2022 Trevor Bayless <trevorbayless1@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from . import MoveListModel, MoveListView
 from cli_chess.game.common import get_piece_unicode_symbol
 from cli_chess.utils import config
