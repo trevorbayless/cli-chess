7632f7f7e2bf071cd0dcc60e1e7c7c39a73701c7
Trevor Bayless <trevorbayless1@gmail.com>
2022-08-20 16:00:58 -0500

    Remove unnecessary looping

--- src/cli_chess/game/board/board_model.py
+++ src/cli_chess/game/board/board_model.py
@@ -92,14 +92,15 @@ class BoardModel:
 
     def get_board_squares(self) -> list:
         """Returns the boards square numbers as a list based current board orientation"""
-        square_numbers = []
-        square_names = []
-
-        for rank in range(len(chess.RANK_NAMES)-1, -1, -1):
-            for file in range(len(chess.FILE_NAMES)):
-                square_index = chess.square(file, rank)
-                square_names.append(chess.square_name(square_index))
-                square_numbers.append(chess.square(file, rank))
+        # Square numbers from white perspective
+        square_numbers = [56, 57, 58, 59, 60, 61, 62, 63,
+                          48, 49, 50, 51, 52, 53, 54, 55,
+                          40, 41, 42, 43, 44, 45, 46, 47,
+                          32, 33, 34, 35, 36, 37, 38, 39,
+                          24, 25, 26, 27, 28, 29, 30, 31,
+                          16, 17, 18, 19, 20, 21, 22, 23,
+                          8, 9, 10, 11, 12, 13, 14, 15,
+                          0, 1, 2, 3, 4, 5, 6, 7]
 
         if self.board_orientation is chess.BLACK:
             return square_numbers[::-1]
