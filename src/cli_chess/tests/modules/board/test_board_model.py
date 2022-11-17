01583497bbac5d7ad8fe2af3519ef796148c3660
Trevor Bayless <trevorbayless1@gmail.com>
2022-11-17 13:47:32 -0600

    Update unit tests

--- src/cli_chess/tests/modules/board/test_board_model.py
+++ src/cli_chess/tests/modules/board/test_board_model.py
@@ -13,124 +13,149 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
+import pytest
 import chess
 from cli_chess.modules.board import BoardModel
 
+# TODO: This will need to be refactored
+#       Instead of board model taking in a dict, pass individual params instead?
+game_parameters_white = {
+    "Variant": "standard",
+    "Side to play as": "white",
+}
 
-class BoardModelTestCase(unittest.TestCase):
-    def test_make_move(self):
-        model = BoardModel()
+game_parameters_black = game_parameters_white.copy()
+game_parameters_black["Side to play as"] = "black"
 
-        # Test valid move
-        try:
-            model.make_move("Nf3")
-        except Exception as e:
-            self.fail(f"test_make_move raised {e}")
 
-        # Test illegal move
-        with self.assertRaises(ValueError):
-            model.make_move("Qe6")
+def test_make_move():
+    model = BoardModel(game_parameters_white)
 
-    def test_get_move_stack(self):
-        pass
+    # Test valid move
+    try:
+        model.make_move("Nf3")
+    except Exception as e:
+        pytest.fail(f"test_make_move raised {e}")
 
-    def test_get_variant_name(self):
-        pass
+    # Test illegal move
+    with pytest.raises(ValueError):
+        model.make_move("Qe6")
 
-    def test_set_board_orientation(self):
-        model = BoardModel()
-        self.assertEqual(model.get_board_orientation(), "white")
 
-        model.set_board_orientation("black")
-        self.assertEqual(model.get_board_orientation(), "black")
+def test_get_move_stack():
+    pass
 
-    def test_get_board_orientation(self):
-        model = BoardModel("black")
-        self.assertEqual(model.get_board_orientation(), "black")
 
-        model = BoardModel("white")
-        self.assertEqual(model.get_board_orientation(), "white")
+def test_get_variant_name():
+    pass
 
-        model.set_board_orientation("black")
-        self.assertEqual(model.get_board_orientation(), "black")
 
-    def test_get_board_squares(self):
-        model = BoardModel()
-        square_numbers = [56, 57, 58, 59, 60, 61, 62, 63,
-                          48, 49, 50, 51, 52, 53, 54, 55,
-                          40, 41, 42, 43, 44, 45, 46, 47,
-                          32, 33, 34, 35, 36, 37, 38, 39,
-                          24, 25, 26, 27, 28, 29, 30, 31,
-                          16, 17, 18, 19, 20, 21, 22, 23,
-                          8, 9, 10, 11, 12, 13, 14, 15,
-                          0, 1, 2, 3, 4, 5, 6, 7]
+def test_random_orientation():
+    pass
 
-        # Test white board orientation
-        self.assertEqual(model.get_board_squares(), square_numbers)
 
-        # Test black board orientation
-        model.set_board_orientation("black")
-        self.assertEqual(model.get_board_squares(), square_numbers[::-1])
+def test_set_board_orientation():
+    model = BoardModel(game_parameters_white)
+    assert model.get_board_orientation() == chess.WHITE
 
-    def test_get_square_file_index(self):
-        model = BoardModel()
-        file_index = model.get_square_file_index(chess.E4)
-        self.assertEqual(file_index, 4)
+    model.set_board_orientation(chess.BLACK)
+    assert model.get_board_orientation() == chess.BLACK
 
-        file_index = model.get_square_file_index(chess.G6)
-        self.assertEqual(file_index, 6)
 
-    def test_get_file_labels(self):
-        model = BoardModel()
-        file_labels = model.get_file_labels()
-        self.assertEqual(file_labels, "a b c d e f g h ")
+def test_get_board_orientation():
+    model = BoardModel(game_parameters_black)
+    assert model.get_board_orientation() == chess.BLACK
 
-        model.set_board_orientation("black")
-        file_labels = model.get_file_labels()
-        self.assertEqual(file_labels, "h g f e d c b a ")
+    model = BoardModel(game_parameters_white)
+    assert model.get_board_orientation() == chess.WHITE
 
-    def test_get_square_rank_index(self):
-        model = BoardModel()
-        rank_index = model.get_square_rank_index(chess.E4)
-        self.assertEqual(rank_index, 3)
+    model.set_board_orientation(chess.BLACK)
+    assert model.get_board_orientation() == chess.BLACK
 
-        rank_index = model.get_square_rank_index(chess.G6)
-        self.assertEqual(rank_index, 5)
 
-    def test_get_rank_label(self):
-        model = BoardModel()
-        rank_index = model.get_square_rank_index(chess.A1)
-        rank_label = model.get_rank_label(rank_index)
-        self.assertEqual(rank_label, "1")
+def test_get_board_squares():
+    model = BoardModel(game_parameters_white)
+    square_numbers = [56, 57, 58, 59, 60, 61, 62, 63,
+                      48, 49, 50, 51, 52, 53, 54, 55,
+                      40, 41, 42, 43, 44, 45, 46, 47,
+                      32, 33, 34, 35, 36, 37, 38, 39,
+                      24, 25, 26, 27, 28, 29, 30, 31,
+                      16, 17, 18, 19, 20, 21, 22, 23,
+                      8, 9, 10, 11, 12, 13, 14, 15,
+                      0, 1, 2, 3, 4, 5, 6, 7]
 
-        rank_index = model.get_square_rank_index(chess.H8)
-        rank_label = model.get_rank_label(rank_index)
-        self.assertEqual(rank_label, "8")
+    # Test white board orientation
+    assert model.get_board_squares() == square_numbers
 
-    def test_is_square_in_check(self):
-        in_check_fen = "8/8/8/8/6K1/8/8/4Q1k1 b - - 21 61"
-        model = BoardModel(fen=in_check_fen)
-        self.assertTrue(model.is_square_in_check(model.board.king(model.board.turn)))
+    # Test black board orientation
+    model.set_board_orientation(chess.BLACK)
+    assert model.get_board_squares() == square_numbers[::-1]
 
-        not_in_check_fen = "8/8/8/3Q4/8/6K1/8/6k1 w - - 21 61"
-        model.board.set_fen(not_in_check_fen)
-        self.assertFalse(model.is_square_in_check(model.board.king(model.board.turn)))
 
-    def test_is_light_square(self):
-        model = BoardModel()
-        self.assertFalse(model.is_light_square(chess.H8))
-        self.assertTrue(model.is_light_square(chess.A2))
-        with self.assertRaises(ValueError):
-            model.is_light_square(100)
+def test_get_square_file_index():
+    model = BoardModel(game_parameters_white)
+    file_index = model.get_square_file_index(chess.E4)
+    assert file_index == 4
 
-    def test_is_white_orientation(self):
-        model = BoardModel()
-        self.assertTrue(model.is_white_orientation())
-        model.set_board_orientation("black")
-        self.assertFalse(model.is_white_orientation())
+    file_index = model.get_square_file_index(chess.G6)
+    assert file_index == 6
 
-        model = BoardModel("black")
-        self.assertFalse(model.is_white_orientation())
-        model.set_board_orientation("white")
-        self.assertTrue(model.is_white_orientation())
+
+def test_get_file_labels():
+    model = BoardModel(game_parameters_white)
+    file_labels = model.get_file_labels()
+    assert file_labels == "a b c d e f g h "
+
+    model.set_board_orientation(chess.BLACK)
+    file_labels = model.get_file_labels()
+    assert file_labels == "h g f e d c b a "
+
+
+def test_get_square_rank_index():
+    model = BoardModel(game_parameters_white)
+    rank_index = model.get_square_rank_index(chess.E4)
+    assert rank_index == 3
+
+    rank_index = model.get_square_rank_index(chess.G6)
+    assert rank_index == 5
+
+
+def test_get_rank_label():
+    model = BoardModel(game_parameters_white)
+    rank_index = model.get_square_rank_index(chess.A1)
+    rank_label = model.get_rank_label(rank_index)
+    assert rank_label == "1"
+
+    rank_index = model.get_square_rank_index(chess.H8)
+    rank_label = model.get_rank_label(rank_index)
+    assert rank_label == "8"
+
+
+def test_is_square_in_check():
+    in_check_fen = "8/8/8/8/6K1/8/8/4Q1k1 b - - 21 61"
+    model = BoardModel(game_parameters_white, fen=in_check_fen)
+    assert model.is_square_in_check(model.board.king(model.board.turn))
+
+    not_in_check_fen = "8/8/8/3Q4/8/6K1/8/6k1 w - - 21 61"
+    model.board.set_fen(not_in_check_fen)
+    assert not model.is_square_in_check(model.board.king(model.board.turn))
+
+
+def test_is_light_square():
+    model = BoardModel(game_parameters_white)
+    assert not model.is_light_square(chess.H8)
+    assert model.is_light_square(chess.A2)
+    with pytest.raises(ValueError):
+        model.is_light_square(100)
+
+
+def test_is_white_orientation():
+    model = BoardModel(game_parameters_white)
+    assert model.is_white_orientation()
+    model.set_board_orientation(chess.BLACK)
+    assert not model.is_white_orientation()
+
+    model = BoardModel(game_parameters_black)
+    assert not model.is_white_orientation()
+    model.set_board_orientation(chess.WHITE)
+    assert model.is_white_orientation()
--- src/cli_chess/tests/modules/test_common.py
+++ src/cli_chess/tests/modules/test_common.py
@@ -15,17 +15,15 @@
 
 from cli_chess.modules.common import *
 from string import ascii_lowercase
-import unittest
 
 
-class CommonTestCase(unittest.TestCase):
-    def test_get_piece_unicode_symbol(self):
-        alphabet = list(ascii_lowercase)
-        for letter in alphabet:
-            if letter in UNICODE_PIECE_SYMBOLS:
-                self.assertEqual(get_piece_unicode_symbol(letter), UNICODE_PIECE_SYMBOLS[letter])
-                self.assertEqual(get_piece_unicode_symbol(letter.upper()), UNICODE_PIECE_SYMBOLS[letter])
-            else:
-                self.assertEqual(get_piece_unicode_symbol(letter), "")
-                self.assertEqual(get_piece_unicode_symbol(letter.upper()), "")
-        self.assertEqual(get_piece_unicode_symbol(""), "")
+def test_get_piece_unicode_symbol():
+    alphabet = list(ascii_lowercase)
+    for letter in alphabet:
+        if letter in UNICODE_PIECE_SYMBOLS:
+            assert get_piece_unicode_symbol(letter) == UNICODE_PIECE_SYMBOLS[letter]
+            assert get_piece_unicode_symbol(letter.upper()) == UNICODE_PIECE_SYMBOLS[letter]
+        else:
+            assert get_piece_unicode_symbol(letter) == ""
+            assert get_piece_unicode_symbol(letter.upper()) == ""
+    assert get_piece_unicode_symbol("") == ""
