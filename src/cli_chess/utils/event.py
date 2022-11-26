003b871361c529a96829162fe75133b982920484
Trevor Bayless <trevorbayless1@gmail.com>
2022-11-26 16:20:19 -0600

    Remove unused event functions

--- src/cli_chess/utils/event.py
+++ src/cli_chess/utils/event.py
@@ -27,22 +27,13 @@ class Event:
 
     def add_listener(self, listener: Callable) -> None:
         """Adds the passed in listener to the notification list"""
-        self.listeners.append(listener)
+        if listener not in self.listeners:
+            self.listeners.append(listener)
 
     def remove_listener(self, listener: Callable) -> None:
         """Removes the passed in listener from the notification list"""
         if listener in self.listeners:
             self.listeners.remove(listener)
-
-    def __iadd__(self, listener: Callable) -> Event:
-        """Allows using += to add a listener"""
-        self.add_listener(listener)
-        return self
-
-    def __isub__(self, listener: Callable) -> Event:
-        """Allows using -= to remove a listener"""
-        self.remove_listener(listener)
-        return self
 
     def notify(self, *args: str, **kwargs: int) -> None:
         """Notifies all listeners of the event"""
