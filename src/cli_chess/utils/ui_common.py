3b239a649a3bb699802925a75c4162e86a06aaf5
Trevor Bayless <trevorbayless1@gmail.com>
2023-01-16 14:30:05 -0600

    Add change_views and repaint_ui

--- src/cli_chess/utils/ui_common.py
+++ src/cli_chess/utils/ui_common.py
@@ -16,7 +16,7 @@
 from prompt_toolkit.mouse_events import MouseEvent, MouseEventType
 from prompt_toolkit.key_binding import KeyPressEvent
 from prompt_toolkit.application import get_app
-from prompt_toolkit.layout import Layout
+from prompt_toolkit.layout import Layout, Container
 from typing import TypeVar, Callable, cast
 
 E = TypeVar("E", bound=Callable[[KeyPressEvent], None])
@@ -26,7 +26,26 @@ T = TypeVar("T", bound=Callable[[MouseEvent], None])
 def go_back_to_main_menu() -> None:
     """Returns to the main menu"""
     from cli_chess.core.startup.startup_presenter import main_presenter
-    get_app().layout = Layout(main_presenter.view, focused_element=main_presenter.view)
+    change_views(main_presenter.view)
+
+
+def change_views(container: Container, focused_element=None):
+    """Change the view to the passed in container.
+       Focuses the view on the optional passed in element.
+    """
+    focused_element = focused_element if focused_element else container
+    get_app().layout = Layout(container)
+
+    try:
+        get_app().layout.focus(focused_element)
+    except ValueError:
+        pass
+
+    repaint_ui()
+
+
+def repaint_ui() -> None:
+    """Force the ui to repaint"""
     get_app().invalidate()
 
 
