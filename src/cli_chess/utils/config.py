f4351f32b00c4f4518e3b208a9a836e80e6de72f
Trevor Bayless <trevorbayless1@gmail.com>
2023-02-05 18:21:42 -0600

    Disable unused engine config

--- src/cli_chess/utils/config.py
+++ src/cli_chess/utils/config.py
@@ -205,7 +205,8 @@ class UiConfig(SectionBase):
 
 
 class EngineConfig(SectionBase):
-    """Creates and manages the "engine" configuration. This configuration can
+    """NOTE: This is not used currently as cli-chess for the time being only directly supports Fairy-Stockfish.
+       Creates and manages the "engine" configuration. This configuration can
        either live in its own file, or be appended as a section by using a
        configuration filename that already exists (such as DEFAULT_CONFIG_FILENAME).
        By default, this will be appended to the default configuration.
@@ -255,5 +256,4 @@ class LichessConfig(SectionBase):
 
 board_config = BoardConfig()
 ui_config = UiConfig()
-engine_config = EngineConfig()
 lichess_config = LichessConfig()
