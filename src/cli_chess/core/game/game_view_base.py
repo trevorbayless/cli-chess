a8d4affb3254433763db6f1b16198dcd286a00e9
Trevor Bayless <trevorbayless1@gmail.com>
2023-02-07 16:21:53 -0600

    Cleanup

--- src/cli_chess/core/game/game_view_base.py
+++ src/cli_chess/core/game/game_view_base.py
@@ -114,7 +114,7 @@ class PlayableGameViewBase(GameViewBase):
                     HSplit([
                         self.player_info_upper_container,
                         self.material_diff_upper_container,
-                        self.move_list_container,  # VSplit([self.move_list_container], align=HorizontalAlign.LEFT),
+                        self.move_list_container,
                         self.material_diff_lower_container,
                         self.player_info_lower_container
                     ])
