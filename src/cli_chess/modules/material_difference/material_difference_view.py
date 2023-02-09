74b0f40587737bbe817bfa5c80854788354a04fd
Trevor Bayless <trevorbayless1@gmail.com>
2023-02-09 13:57:40 -0600

    Fix material diff output for horde

--- src/cli_chess/modules/material_difference/material_difference_view.py
+++ src/cli_chess/modules/material_difference/material_difference_view.py
@@ -14,7 +14,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import annotations
-from prompt_toolkit.layout import ConditionalContainer, D
+from prompt_toolkit.layout import Container, ConditionalContainer, Window, HSplit, D
 from prompt_toolkit.filters import to_filter
 from prompt_toolkit.widgets import TextArea
 from typing import TYPE_CHECKING
@@ -25,6 +25,7 @@ if TYPE_CHECKING:
 class MaterialDifferenceView:
     def __init__(self, presenter: MaterialDifferencePresenter, initial_diff: str, show: bool = True):
         self.presenter = presenter
+        self.show = show
         self._diff_text_area = TextArea(text=initial_diff,
                                         style="class:material-difference",
                                         width=D(min=1),
@@ -33,12 +34,22 @@ class MaterialDifferenceView:
                                         focusable=False,
                                         multiline=False,
                                         wrap_lines=False)
-        self.show = show
+        self._container = self._create_container()
+
+    def _create_container(self):
+        """Creates the container for the material difference. Handles providing
+           an empty container if the view should be hidden. This allows for
+           the container display formatting to remain consistent
+        """
+        return HSplit([
+            ConditionalContainer(self._diff_text_area, to_filter(self.show)),
+            ConditionalContainer(Window(height=D(max=1)), to_filter(not self.show))
+        ])
 
     def update(self, difference: str) -> None:
         """Updates the view output with the passed in text"""
         self._diff_text_area.text = difference
 
-    def __pt_container__(self) -> ConditionalContainer:
+    def __pt_container__(self) -> Container:
         """Returns this views container"""
-        return ConditionalContainer(self._diff_text_area, to_filter(self.show))
+        return self._container
