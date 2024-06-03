12c6c3c9a1ae16fc6ca098bfa059772c866d185a
Trevor Bayless <3620552+trevorbayless@users.noreply.github.com>
2024-06-03 17:05:35 -0500

    Remove license headers

--- src/cli_chess/__metadata__.py
+++ src/cli_chess/__metadata__.py
@@ -1,22 +1,7 @@
-# Copyright (C) 2021-2024 Trevor Bayless <trevorbayless1@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 __name__ = "cli-chess"
 __version__ = "1.2.0"
 __description__ = "A highly customizable way to play chess in your terminal"
 __url__ = "https://github.com/trevorbayless/cli-chess"
 __author__ = "Trevor Bayless"
-__author_email__ = "trevorbayless1@gmail.com"
+__author_email__ = "cli-chess@trb.simplelogin.com"
 __license__ = "GPL-3.0+"
