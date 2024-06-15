e1febf0ae8e6aa730b3ba65cc5ae3049183b1172
Trevor Bayless <3620552+trevorbayless@users.noreply.github.com>
2024-06-15 16:39:46 -0500

    Bump version

--- src/cli_chess/__metadata__.py
+++ src/cli_chess/__metadata__.py
@@ -1,5 +1,5 @@
 __name__ = "cli-chess"
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __description__ = "A highly customizable way to play chess in your terminal"
 __url__ = "https://github.com/trevorbayless/cli-chess"
 __author__ = "Trevor Bayless"
